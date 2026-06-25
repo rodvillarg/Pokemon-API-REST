86e2c37141ec461e6cd6163e6dba95a346eb5552
rodvillarg <jesus.villegas261186@potros.itson.edu.mx>
2026-06-25 14:54:03 -0700

    Delete main directory

--- main/java/com/example/PokemonAPI/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.PokemonAPI.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
